ba1ecf98fdf7b06f1e59e4f258bbee68ce612aa9
Tiurabe <63129379+Tiurabe@users.noreply.github.com>
2021-09-09 11:13:30 -0300

    Create fe_scripts_fix.lua

---
+++ fe_scripts_fix.lua
@@ -0,0 +1,37 @@
+--Credits to whoever made this, got from mizt hub so probably made by them.
+local realfenv
+realfenv = hookfunction(getgenv().getfenv,function(a)
+    local num = tonumber(a)
+    if num then
+        if num > 10 or num < 1 then
+            error("Bypassing the error XD")
+        else
+            return realfenv()
+        end
+    else
+        return realfenv()
+    end
+end)
+
+local maxSim = 1000
+getgenv().setsimulationradius = newcclosure(function(i,v)
+    local plr = game.Players.LocalPlayer
+    sethiddenproperty(plr,"MaxSimulationRadius",maxSim)
+    sethiddenproperty(plr,"SimulationRadius",maxSim)
+end)
+
+local sethidden
+sethidden = hookfunction(getgenv().sethiddenproperty,function(i,p,v)
+    local plr = game.Players.LocalPlayer
+    if i == plr then
+        if (p == "MaxSimulationRadius" or p == "SimulationRadius") then
+            if v == maxSim then
+                sethidden(i,p,v)
+            end
+        else
+            sethidden(i,p,v)
+        end
+    else
+        sethidden(i,p,v)
+    end
+end)
